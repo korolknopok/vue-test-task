dd3bfb14e5570527b2390a9405cc118bd487dd4c
Назмутдинов Артём <94155787+korolknopok@users.noreply.github.com>
2025-07-10 16:02:19 +0700

    feature(add-and-delete-record): Add and delete record
    Co-authored-by: Артём <fedor.artem.ivanov@mail.ru>

--- src/components/AccountManagement.vue
+++ src/components/AccountManagement.vue
@@ -1,10 +1,44 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+interface Account {
+  labels: string;
+  type: string;
+  login: string;
+  password: string | null;
+}
+
+const accounts = ref<Account[]>([
+  {
+    labels: '',
+    type: 'Локальная',
+    login: '',
+    password: ''
+  }
+]);
+
+const addAccount = () => {
+  accounts.value.push({
+    labels: '',
+    type: 'Локальная',
+    login: '',
+    password: ''
+  });
+};
+
+const removeAccount = (index: number) => {
+  accounts.value.splice(index, 1);
+};
+
+</script>
+
 <template>
   <v-container>
     <v-row align="center">
       <v-col>
         <v-row align="center" style="gap: 10px; margin: 0">
-          <h1>Учётная запись</h1>
-          <v-btn class="text-left">
+          <h1>Учётные записи</h1>
+          <v-btn @click="addAccount" class="text-left">
             <v-icon>mdi-plus</v-icon>
           </v-btn>
         </v-row>
@@ -22,21 +56,41 @@
       </v-col>
     </v-row>
 
-    <v-row style="opacity: 0.5">
-      <v-col cols="3">Метки</v-col>
-      <v-col cols="3">Тип записи</v-col>
-      <v-col cols="3">Логин</v-col>
-      <v-col cols="2">Пароль</v-col>
-      <v-col cols="1"></v-col>
+    <v-row v-for="(account, index) in accounts" :key="index">
+      <v-col cols="12">
+        <v-card>
+          <v-card-text>
+            <v-row>
+              <v-col cols="12" sm="3">
+                <v-text-field
+                    label="Метки"
+                    placeholder="например, admin;user"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="3">
+                <v-select
+                    :items="['LDAP', 'Локальная']"
+                    label="Тип записи"
+                ></v-select>
+              </v-col>
+              <v-col cols="12" sm="3">
+                <v-text-field label="Логин"></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="2">
+                <v-text-field label="Пароль" type="password"></v-text-field>
+              </v-col>
+
+              <v-col cols="12" sm="1" class="text-right">
+                <v-btn icon @click="removeAccount(index)">
+                  <v-icon>mdi-delete</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 
-<script setup lang="ts">
-import {defineComponent} from "vue";
-
-defineComponent({
-  name: "AccountManagement",
-})
-</script>
