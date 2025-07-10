e75bc42ae78dda3c25ed9f6ece408510e1a3f59b
Артём <fedor.artem.ivanov@mail.ru>
2025-07-10 17:26:11 +0700

    feature(add-state-manager): add pinia state manager

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
+import {createPinia} from "pinia";
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
 const app = createApp(App);
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
 app.use(vuetify);
 app.use(router);
 app.mount('#app');
